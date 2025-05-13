8e12ae83f49170f2dc07e9f7ca7b86f994d84d42
Yahuy1525 <huytrian2005@gmail.com>
2025-05-13 10:45:33 +0700

    Backend Folder Structures added. Freelancer model created

---
+++ backend/src/models/FreelancerModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+const FreelancerSchema = new mongooese.Schema({
+    wallet: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    skills: {
+        type: Array,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    }
+})
+
+module.exports = mongoose.module("Freelancer", FreelancerSchema)
