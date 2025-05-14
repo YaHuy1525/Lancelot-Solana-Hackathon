d9ca3b6d124131a881f8adde56b50617fa5cab1a
Kong Dinh <alexvinh2911@gmail.com>
2025-05-14 11:33:46 +0700

    Add Solana wallet integration and styles; refactor App and HomePage components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
 import React from "react";
+import { WalletConfig } from "./config/wallet";
+import { WalletProvider } from "./contexts/WalletContext";
 import HomePage from "./pages/HomePage";
 import "./styles/index.css";
 // import "@fortawesome/fontawesome-free/css/all.min.css";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <WalletConfig>
+      <WalletProvider>
+        <div className="App">
+          <HomePage />
+        </div>
+      </WalletProvider>
+    </WalletConfig>
   );
-}
+};
 
 export default App;
